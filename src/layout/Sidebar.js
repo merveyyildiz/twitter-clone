7f412b57eb548988fd58aaeded093f167ad1c189
merveyyildiz <merve.yildiz@demirorenmedya.com>
2022-09-20 23:56:39 +0300

    iconlar eklendi

--- src/layout/Sidebar.js
+++ src/layout/Sidebar.js
@@ -1,8 +1,48 @@
 import React from "react";
-
+import {
+  BookmaksIcon,
+  EmojiIcon,
+  ExploreIcon,
+  GIFIcon,
+  HomeIcon,
+  ImageIcon,
+  LikeIcon,
+  ListsIcon,
+  MessagesIcon,
+  MoreIcon,
+  NotificationIcon,
+  PollIcon,
+  ProfileIcon,
+  ReplyIcon,
+  ReTweetIcon,
+  ScheduleIcon,
+  SearchIcon,
+  ShareIcon,
+} from "../assets/icons/icon";
 const Sidebar = () => {
-    return (
-        <div className="w-72 bg-blue-200">sidebar</div>
-    )
-}
+  return (
+    <div className="w-72 bg-blue-200">
+      <HomeIcon />
+      <ExploreIcon />
+      <NotificationIcon />
+      <MessagesIcon />
+      <BookmaksIcon />
+      <ListsIcon />
+      <ProfileIcon />
+      <MoreIcon />
+
+      <GIFIcon />
+      <ImageIcon/>
+      <PollIcon />
+      <EmojiIcon/>
+      <LikeIcon />
+      <ShareIcon />
+      <ReTweetIcon />
+      <ScheduleIcon />
+      <ReplyIcon />
+      <SearchIcon />
+      sidebar
+    </div>
+  );
+};
 export default Sidebar;
